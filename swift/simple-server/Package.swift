b822d268b85e5880034ff6f20e7bb09bfa2a0df1
Joannis Orlandos <joannis@orlandos.nl>
2026-01-29 00:58:23 +0100

    Disable swift-configuration

--- swift/simple-server/Package.swift
+++ swift/simple-server/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 
 import PackageDescription
 
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v14)
     ],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0", traits: []),
         .package(url: "https://github.com/apple/swift-container-plugin", from: "1.0.0"),
     ],
     targets: [
