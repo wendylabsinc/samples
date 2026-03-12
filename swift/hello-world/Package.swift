60c34ab24486e63f1aef137feb0cf2a8c2396a3d
Joannis Orlandos <joannis@orlandos.nl>
2026-03-12 20:25:37 +0100

    Expose default product

--- swift/hello-world/Package.swift
+++ swift/hello-world/Package.swift
@@ -5,12 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "HelloWorld",
+    products: [
+        .executable(name: "HelloWorld", targets: ["HelloWorld"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-container-plugin", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "HelloWorld"
         ),
